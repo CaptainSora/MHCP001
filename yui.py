761f23eedd67645adac22d8ede3d4d9e1868ec48
Joseph Wang <joseph4x4@gmail.com>
2020-06-10 21:18:50 -0400

    Added messages

--- yui.py
+++ yui.py
@@ -1,4 +1,5 @@
 from discord import Client
+from asyncio import sleep
 
 client = Client()
 GUILD = "Asteros"
@@ -18,9 +19,20 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if ".poke" in message.content:
+
+    if message.content == ".poke":
         await message.channel.send("No poking.")
+    elif message.content == ".taco":
+        await message.channel.send("I love spicy tacos!")
+    elif message.content == ".spicytaco":
+        await message.channel.send("Mm!")
+    elif message.content == ".firewall":
+        await message.channel.send("...bypassing security...")
+        message = await message.channel.send("...10%...")
+        for i in range (2, 11):
+            await sleep(1)
+            await message.edit(content=f"...{i}0%...")
+        message = await message.channel.send("Security clearance granted!")
 
 with open("token.txt") as f:
     token = f.read()
